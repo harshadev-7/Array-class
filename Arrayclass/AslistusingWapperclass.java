77e7ef740a8c8727ec5e5f89b1488604330997f0
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-09 14:20:53 +0530

    Second commit

--- Arrayclass/AslistusingWapperclass.java
+++ Arrayclass/AslistusingWapperclass.java
@@ -4,7 +4,7 @@ public class AslistusingWapperclass {
     public static void main(String[] args) {
         int i;
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter the size of an array");
+        System.out.println("enter the size of an array:");
         int n = scan.nextInt();
         Integer arr[] = new Integer[n];
         System.out.println("enter the elements in an array");
